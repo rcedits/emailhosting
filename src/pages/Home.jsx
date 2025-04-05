54adb2e25b0b39ad4be9b962b329b0abe53df4a8
Ramchandra Chaugule <ramchandrachaugule4@gmail.com>
2025-04-05 13:11:34 +0530

    add swiper

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,16 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import PostCard from "../components/PostCard";
 
-// Import Owl Carousel and its styles
-// import "owl.carousel/dist/assets/owl.carousel.css";
-// import "owl.carousel/dist/assets/owl.theme.default.css";
-// import OwlCarousel from "react-owl-carousel";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import banner1 from '/images/slider1.jpg';
+import banner2 from '/images/slider2.jpg';
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
@@ -23,65 +29,69 @@ const Home = () => {
   }, [page]);
 
   return (
-    <div className="container">
-
-      {/* === Owl Carousel Slider Section === */}
-      {/* <OwlCarousel
-        className="owl-theme"
-        loop
-        margin={10}
-        nav
-        autoplay
-        autoplayTimeout={3000}
-        dots={false}
-      >
-        <div className="item">
-          <img src="/images/slider1.jpg" alt="Slider 1" className="img-fluid rounded" />
-        </div>
-        <div className="item">
-          <img src="/images/slider2.jpg" alt="Slider 2" className="img-fluid rounded" />
-        </div>
-        <div className="item">
-          <img src="/images/slider3.jpg" alt="Slider 3" className="img-fluid rounded" />
-        </div>
-      </OwlCarousel> */}
-
-      {/* === Main Content Section === */}
-      <h1 className="mt-4">Welcome to Email Hosting</h1>
-      <p>We provide reliable and secure email hosting services.</p>
-      <p>Our email hosting services include:</p>
-      <ul>
-        <li>Custom domain email addresses</li>
-        <li>Advanced spam and virus protection</li>
-        <li>24/7 customer support</li>
-        <li>High availability and uptime</li>
-        <li>Easy integration with other tools and services</li>
-      </ul>
-
-      <div className="row">
-        {posts.map((post) => (
-          <div key={post.id} className="col-md-4 mb-4">
-            <PostCard post={post} />
-          </div>
-        ))}
-      </div>
-
-      <div className="d-flex justify-content-center mt-3">
-        <button
-          className="btn btn-primary me-2"
-          disabled={page === 1}
-          onClick={() => setPage(page - 1)}
+    <>
+      {/* === Swiper Section === */}
+      <div className="section">
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+          spaceBetween={30}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          autoplay={{ delay: 3000 }}
+          loop={true}
         >
-          Previous
-        </button>
-        <span className="align-self-center">Page {page}</span>
-        <button className="btn btn-primary ms-2" onClick={() => setPage(page + 1)}>
-          Next
-        </button>
+          <SwiperSlide>
+            <img src={banner1} alt="Banner 1" className="w-100" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={banner2} alt="Banner 2" className="w-100" />
+          </SwiperSlide>
+        </Swiper>
       </div>
-
-    </div>
+  
+      {/* === Main Content Section === */}
+      <div className="container">
+        <h1 className="mt-4">Welcome to Email Hosting</h1>
+        <p>We provide reliable and secure email hosting services.</p>
+        <p>Our email hosting services include:</p>
+        <ul>
+          <li>Custom domain email addresses</li>
+          <li>Advanced spam and virus protection</li>
+          <li>24/7 customer support</li>
+          <li>High availability and uptime</li>
+          <li>Easy integration with other tools and services</li>
+        </ul>
+  
+        <div className="row">
+          {posts.map((post) => (
+            <div key={post.id} className="col-md-4 mb-4">
+              <PostCard post={post} />
+            </div>
+          ))}
+        </div>
+  
+        <div className="d-flex justify-content-center mt-3">
+          <button
+            className="btn btn-primary me-2"
+            disabled={page === 1}
+            onClick={() => setPage(page - 1)}
+          >
+            Previous
+          </button>
+          <span className="align-self-center">Page {page}</span>
+          <button
+            className="btn btn-primary ms-2"
+            onClick={() => setPage(page + 1)}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </>
   );
+  
 };
 
 export default Home;
